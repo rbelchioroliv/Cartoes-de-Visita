383cbbb687c4e1a226e49ab5ec61237bc0db28c2
rbelchioroliv <54287085+rbelchioroliv@users.noreply.github.com>
2023-09-21 14:17:32 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,34 @@
+
+let mapaExibido = false;
+let map;
+let mapButton = document.getElementById('mapButton');
+let mapContainer = document.querySelector('.map-container'); // Seleciona a div do mapa pela classe
+
+function exibirMapa() {
+    if (mapaExibido) {
+        // Se o mapa já estiver sendo exibido, remova-o
+        if (map) {
+            map.remove();
+        }
+        mapaExibido = false;
+        mapButton.value = '🗺️Como Chegar'; // Altera o texto do botão
+        mapContainer.style.display = 'none'; // Esconde a div do mapa
+    } else {
+        // Se o mapa não estiver sendo exibido, crie-o
+        map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // Adicionar um marcador no mapa
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('Estou aqui!!')
+            .openPopup();
+
+        mapaExibido = true;
+        mapButton.value = '🙈Esconder Mapa'; // Altera o valor do input
+        mapContainer.style.display = 'block'; // Mostra a div do mapa
+    }
+}
+
